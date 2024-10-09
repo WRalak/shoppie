5b327ece7dcfdbb02f2123a7a02906f9fea9a18a
WRalak <140385716+WRalak@users.noreply.github.com>
2024-10-09 16:09:26 +0300

    first commit

---
+++ shoppie/src/App.jsx
@@ -0,0 +1,25 @@
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import Navbar from './components/Navbar'
+   import Cart from './pages/cart/cart'
+   import Shop from './pages/shop/shop'
+  import Contact from './pages/contact'
+import { ShopContextProvider } from './context/shop-context'
+
+const App = () => {
+  return (
+    <div className='App'>
+      <ShopContextProvider> <Router>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element ={<Shop/>}/>
+          <Route path="/contact" element={<Contact />} />
+          <Route path='/cart' element={<Cart/>}/>
+        </Routes>
+      </Router>
+      </ShopContextProvider>
+      
+    </div>
+  )
+}
+
+export default App
